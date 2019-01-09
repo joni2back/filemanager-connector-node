2796493ae4c50d76762532ddf91e17007676204c
Jonas Sciangula Street <joni2back@gmail.com>
2019-01-09 14:44:19 -0300

    0.0.3

--- app.js
+++ app.js
@@ -1,48 +1,70 @@
 const express = require('express');
 const app = express();
 const fs = require('fs');
+var multer  = require('multer')
 
 app.use(express.json());
+
+const apiResponse = (res, status = 200) => (data, success = true, errorMsg = null, error = null) => {
+    return res.status(status).json({
+        data,
+        success,
+        errorMsg,
+        error
+    });
+};
+
+const apiError = (res, status = 500) => (errorMsg = null, error = null) => apiResponse(res, status)(null, false, errorMsg, error);
 
 app.use(function(req, res, next) {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type,path');
+    res.setHeader('Access-Control-Expose-Headers', 'Content-Disposition');
     res.setHeader('Access-Control-Allow-Credentials', true);
     next();
 });
 
 app.get('/filemanager/list', (req, res) => {
-    let path = req.query.path || '/';
-    fs.readdir(path, function(err, items) {
+    const path = req.query.path || '/';
+    let items = [];
 
-        return res.json((items && items.length && items || []).map(f => {
+    fs.readdir(path, (err, files) => {
+        if (err) {
+            return apiError(res)('Cannot read folder', err);
+        }
+
+        items = (files || []).map(f => {
             const fpath = path + '/' + f;
             let type = 'file';
             try {
                 type = fs.statSync(fpath).isDirectory() ? 'dir' : type;
             } catch (err) {
-
             }
             return {
                 name: f,
-                type: type
+                type
             }
-        }));
+        });
+
+        return apiResponse(res)(items);
     });
+
 });
 
 
 app.post('/filemanager/dir/create', (req, res) => {
-
     const fullPath = req.body.path + '/' + req.body.directory;
     
-    let result = false;
-    if (!fs.existsSync(fullPath)){
-        result = fs.mkdirSync(fullPath);
-        return res.json(result || true);
+    if (fs.existsSync(fullPath)) {
+        return apiError(res)('The folder already exist', err);
     }
-    
+    try {
+        result = fs.mkdirSync(fullPath);
+        return apiResponse(res)(result);
+    } catch(err) {
+        return apiError(res)('Unknown error creating folder', err);
+    }
 });
 
 
@@ -50,7 +72,6 @@ app.get('/filemanager/file/content', (req, res) => {
     let path = req.query.path;
     return res.download(path);
 });
-
 
 
 app.post('/filemanager/items/copy', (req, res) => {
@@ -74,16 +95,9 @@ app.post('/filemanager/items/copy', (req, res) => {
     });
 
     Promise.all(promises).then(values => {
-        res.json({
-            success: true,
-            result: values
-        });
-    }).catch(values => {
-        res.status(500).json({
-            success: false,
-            errorMsg: 'An error ocurred copying files',
-            error: values,
-        });
+        return apiResponse(res)(values);
+    }).catch(err => {
+        return apiError(res)('An error ocurred copying files', err);
     });
 });
 
@@ -108,23 +122,39 @@ app.post('/filemanager/items/move', (req, res) => {
     });
 
     Promise.all(promises).then(values => {
-        res.json({
-            success: true,
-            result: values
-        });
-    }).catch(values => {
-        res.status(500).json({
-            success: false,
-            errorMsg: 'An error ocurred moving files',
-            error: values,
-        });
+        return apiResponse(res)(values);
+    }).catch(err => {
+        return apiError(res)('An error ocurred moving files', err);
     });
 });
 
 
+app.post('/filemanager/items/upload', (req, res, next) => {
+    const upload = multer({
+        storage: multer.diskStorage({
+            destination: (req, file, cb) => {
+                // we pass the path by headers because is not present in params at this point
+                cb(null, req.headers.path);
+            },
+            filename: (req, file, cb) => {
+                cb(null, file.originalname);
+            }
+        })
+    }).array('file[]');
+
+    upload(req, res, err => {
+        if (err) {
+            return apiError(res)('An error occurred uploading files', err);
+        }
+        if (! req.files.length) {
+            return apiError(res)('Cannot find any file to upload');
+        }
+        return apiResponse(res)(true);
+    });
+});
+
 app.post('/filemanager/items/remove', (req, res) => {
     const { path, filenames, recursive } = req.body;
-
     const promises = (filenames || []).map(f => {
         const fullPath = path + '/' + f;
         return new Promise((resolve, reject) => {
@@ -142,16 +172,9 @@ app.post('/filemanager/items/remove', (req, res) => {
     });
 
     Promise.all(promises).then(values => {
-        res.json({
-            success: true,
-            result: values
-        });
-    }).catch(values => {
-        res.status(500).json({
-            success: false,
-            errorMsg: 'An error ocurred deleting file',
-            error: values,
-        });
+        return apiResponse(res)(values);
+    }).catch(err => {
+        return apiError(res)('An error ocurred deleting file', err);
     });
     
 });
